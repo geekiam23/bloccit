079c98cc7bfa198efe2533b700c48677b4c06e77
Will Larry <will23larry@gmail.com>
2017-05-31 17:53:09 -0500

    Checkpoint-18: In progress

---
+++ spec/controllers/posts_controller_spec.rb
@@ -0,0 +1,20 @@
+require 'rails_helper'
+
+RSpec.describe PostsController, type: :controller do
+
+    let(:my_post) { Post.create!(title: RandomData.random_sentence, body:)}
+  describe "GET #index" do
+    it "returns http success" do
+      get :index
+      expect(response).to have_http_status(:success)
+    end
+
+    it "assigns [my_post] to @posts" do
+        get:index
+        expect(assigns(:posts)).to eq([my_post])
+    end
+  end
+
+  
+
+end
