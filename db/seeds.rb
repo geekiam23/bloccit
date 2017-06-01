76200d97046e98682107e10bffa516975ee51037
Will Larry <will23larry@gmail.com>
2017-05-31 21:04:32 -0500

    Fixed Merge Errors

--- db/seeds.rb
+++ db/seeds.rb
@@ -17,12 +17,10 @@ posts = Post.all
     )
 end
 
-puts "#{Post.count}"
 Post.find_or_create_by!(
     title: 'What do you think about Bloccit?',
     body: 'I love it so far!'
 )
-puts "#{Post.count}"
 
 puts "Seed finished"
 puts "#{Post.count} posts created"
