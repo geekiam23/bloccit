3efd0938a414d28ee0194dafcad9f2a8e8301fa6
Will Larry <will23larry@gmail.com>
2017-06-23 16:03:50 -0500

    Assignment-28: Completed

--- app/helpers/topics_helper.rb
+++ app/helpers/topics_helper.rb
@@ -3,7 +3,11 @@ module TopicsHelper
     current_user && current_user.admin?
   end
 
-  def admin_is_authorized_for_sponsored_post?
-    current_user && current_user.admin?
+  def admin_is_authorized_for_topics?
+    current_user.admin?
+  end
+
+  def moderator_is_authorized_for_topics?
+    current_user && current_user.moderator? || current_user.admin?
   end
 end
