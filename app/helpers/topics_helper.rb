8373702fb4dab2cfd0f17ee849d8e76e3fa98fb4
Will Larry <will23larry@gmail.com>
2017-06-23 14:04:27 -0500

    Checkpoint-28: Completed

--- app/helpers/topics_helper.rb
+++ app/helpers/topics_helper.rb
@@ -1,2 +1,9 @@
 module TopicsHelper
+  def user_is_authorized_for_topics?
+    current_user && current_user.admin?
+  end
+
+  def admin_is_authorized_for_sponsored_post?
+    current_user && current_user.admin?
+  end
 end
