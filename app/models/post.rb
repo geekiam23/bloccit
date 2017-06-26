a6fae0ee4d7276178a80c0c0d3148e9cf87eaa9f
Will Larry <will23larry@gmail.com>
2017-06-26 10:37:11 -0500

    Checkpoint-31: Completed

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,6 +3,7 @@ class Post < ActiveRecord::Base
   belongs_to :user
   has_many :comments, dependent: :destroy
   has_many :votes, dependent: :destroy
+  has_many :favorites, dependent: :destroy
 
   after_create :create_vote
 
@@ -34,7 +35,7 @@ class Post < ActiveRecord::Base
   end
 
   private
-  
+
   def create_vote
     user.votes.create(value: 1, post: self)
   end
